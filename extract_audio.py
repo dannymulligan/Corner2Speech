c483fcf1a22cf5298df93df09e782912398f5d81
Danny Mulligan <danny@dannymulligan.com>
2023-08-24 20:09:33 -0500

    Added all of the Spa tracks, with volume normalized corner names

---
+++ extract_audio.py
@@ -0,0 +1,40 @@
+﻿#!python3
+
+# Before you can use this script, you must install pydub & ffmpeg
+# Install pydub with "pip install pydub"
+# Installing ffmpeg is a bit harder, see https://phoenixnap.com/kb/ffmpeg-windows
+
+import argparse
+import csv
+import os
+from pydub import AudioSegment
+
+parser = argparse.ArgumentParser(prog="extract_audio.py",
+            description="extract audio from source files as specified by a CSV file")
+parser.add_argument("CSVFilename", help="CSV file")
+args = parser.parse_args()
+
+PreviousSource = None
+
+print("Processing clips specified in {}".format(args.CSVFilename))
+if not os.path.isfile(args.CSVFilename):
+    print("ERROR: '{}' doesn't exist".format(args.CSVFilename))
+else:
+    #csvfile = open(args.CSVFilename)
+    #csvreader = csv.reader(csvfile)
+    csvreader = csv.reader(open(args.CSVFilename))
+    for clip in csvreader:
+        if len(clip) == 0:
+            continue
+        SourceFilename, StartTime, EndTime, LevelAdjust, OutputFile = \
+            clip[0], float(clip[1]), float(clip[2]), float(clip[3]), clip[4]
+        print(SourceFilename, StartTime, EndTime, LevelAdjust, OutputFile)
+
+        if not(PreviousSource == SourceFilename):
+            print("Opening {}".format(SourceFilename))
+            SourceAudio = AudioSegment.from_file(SourceFilename, "wav")
+            PreviousSource = SourceFilename
+
+        AudioClip = SourceAudio[int(1000*StartTime):int(1000*EndTime)]
+        #AudioClip = SourceAudio[int(1000*StartTime):int(1000*EndTime)] + LevelAdjust
+        AudioClip.export(OutputFile, format="wav")
---
+++ measure_loudness.py
@@ -0,0 +1,23 @@
+﻿#!python3
+
+# You may need to run "pip install pyloudnorm" before you can run this code
+# You may need to run "pip install pysoundfile" before you can run this code
+
+import os
+import argparse
+import soundfile
+import pyloudnorm
+
+parser = argparse.ArgumentParser(prog="measure_loudness.py",
+            description="measure and report the loudness of an audio file")
+parser.add_argument("filenames", help="audio file", nargs="*")
+args = parser.parse_args()
+
+for FileName in args.filenames:
+    if not os.path.isfile(FileName):
+        print("Warning: '{}' not found".format(FileName))
+    else:
+        data, rate = soundfile.read(FileName)
+        meter = pyloudnorm.Meter(rate)
+        loudness = meter.integrated_loudness(data)
+        print("rate = {:,}Hz, loudness = {:4.1f}dB: '{}'".format(rate, loudness, FileName))
